817516269a43d44b91f09723eecc719432be2538
Leonardo Alchieri <leonardo@alchieri.eu>
2022-05-25 10:40:22 +0200

    Added setup and updated README for publication ⚙️

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# execution-time-wrapper
-A simple package with a few method to get the execution time of methods through wrapping
+# Execution Time Wrapper
+
+A simple package with a few method to get the execution time of methods through wrapping.
+
+## Usage
+
+Currently, the package contains two methods, one which returns to sdout (print) and one which goes to a logger.
+
+```python
+from execution_time import get_execution_time_log
+
+@get_execution_time_log
+def my_fun():
+    print("Hello World!")
+```
+The same for the other method given, i.e. `get_execution_time_print`.
+
+## TODOs
+
+[ ] Implement logger level
+
+@2022, Leonardo Alchieri
+
+<sub>People-Centered Computing Lab - [Università della Svizzera italiana, Switzerland](https://www.usi.ch/en)</sub>
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+# NOTE: necessary to import __version__
+# exec(open("apple_heartrate_pandas/_version.py").read())
+
+from os import path
+with open(path.join(path.abspath(path.dirname(__file__)), "README.md")) as f:
+    readme = f.read()
+
+setup(
+    name='execution_time_wrapper',
+    version="0.1.1",
+    license='MIT',
+    url='https://github.com/LeonardoAlchieri/ExecutionTimeWrapper',
+    author='Leonardo Alchieri',
+    author_email='leonardo@alchieri.eu',
+    description='A Python package with some simple wrapper functions to print the execution time of methods',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    keywords=['time', 'wrapper', 'log', 'print', 'execution', 'speed'],
+    packages=['execution_time_wrapper'],
+    # py_modules=['apple_heartrate_pandas'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 4 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+    'python_version>="3.7"',
+]
+)
